884f86d7c8f41317ee3c53bd21f5b8f8e0aa5c2d
Guilherme <guibbs11032003@gmail.com>
2024-07-24 13:35:31 -0300

    Att Header Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className={mainFontFamily.className}>
+      <head>
+        <link rel="shortcut icon" href="./icons/favicon.ico" type="image/x-icon" />
+      </head>
       <body>{children}</body>
     </html>
   );
