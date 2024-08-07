78104bfbbf0e01b47604d18ea333f826c5c66f48
Guilherme <guibbs11032003@gmail.com>
2024-08-07 17:41:09 -0300

    Att Contact Finished

--- src/layout/docs/Contact.tsx
+++ src/layout/docs/Contact.tsx
@@ -1,7 +1,38 @@
+import Button from "@/components/Button";
+
+import contactImage from "/public/assets/contact/contact_image.png"
+
 export default function Contact() {
     return (
-        <section>
-
+        <section className="contact_container">
+            <div className="contact_query" style={{backgroundImage: `url('${contactImage.src}')`}}>
+                <div className="contact_query_content">
+                    <p className="contact_query_content_paragraph">
+                        Peça seu orçamento agora!
+                    </p>
+                    <h1 className="contact_query_content_heading">
+                        Interessado em Nossos Serviços?
+                    </h1>
+                </div>
+            </div>
+            <div className="contact_action">
+                <div className="contact_action_content">
+                    <h3 className="contact_action_content_heading">
+                        Solicite um orçamento
+                    </h3>
+                    <p className="contact_action_content_paragraph">
+                        Quer saber mais sobre nossos serviços? Clique no botão abaixo e receba um
+                        orçamento personalizado de forma rápida e fácil. Estamos prontos para ajudar!
+                    </p>
+                    <Button
+                        text='Faça seu Orçamento!'
+                        backgroundColor="#2FE076"
+                        borderRadius="0 1.3rem 0 1.3rem"
+                        color="#012531"
+                        maxWidth
+                    />
+                </div>
+            </div>
         </section>
     )
 }
