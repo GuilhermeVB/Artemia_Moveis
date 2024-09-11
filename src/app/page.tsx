50dafcc654a230e2926468047c37d9ff6fecf2b9
Guilherme <guibbs11032003@gmail.com>
2024-09-10 21:16:22 -0300

    Att Docs Delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Main from "@/layout/docs/Main"
-import Footer from "@/layout/footer/Footer"
+import AboutUs from "@/layout/aboutus/AboutUs";
+import Banner from "@/layout/banner/Banner";
+import Contact from "@/layout/contact/Contact";
+import Feedback from "@/layout/feedback/Feedback";
+import Footer from "@/layout/footer/Footer";
+import Portfolio from "@/layout/portfolio/Portfolio";
 
 export default function Home() {
   return (
     <>
-      <Main />
+      <Banner />
+      <Portfolio />
+      <AboutUs />
+      <Feedback />
+      <Contact />
       <Footer />
     </>
   )
