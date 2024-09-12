7ca201b6d7e173ae69036b6e868138554b086356
Guilherme <guibbs11032003@gmail.com>
2024-09-11 22:16:23 -0300

    Att WhatsAppFloater

---
+++ src/components/whatsappfloater/WhatsAppFloater.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+export default function WhatsApp() {
+
+  return (
+    <div className='floating_whats_app'>
+      <FloatingWhatsApp accountName='Artêmia Móveis' phoneNumber='75983354685' />
+    </div>
+  )
+}
